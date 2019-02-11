a632684b7c6703dee3a8ee2e2229dff69e621a5e
Christophe Bautista <chrastophe@gmail.com>
2019-02-11 17:48:22 +0100

    Mise a forme de la page annonce

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
 {
     /**
-     * @Route("/ad", name="ad")
+     * @Route("/ads", name="ads_index")
      */
     public function index()
     {
+        $repo = $this->getDoctrine()->getRepository(Ad::class);
+
+        $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads,
         ]);
     }
 }
