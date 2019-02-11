08e7105f55703c888ae028554820cd9e61e4f81a
Christophe Bautista <chrastophe@gmail.com>
2019-02-11 16:04:27 +0100

    Ajouter de la fonction slug dans AppFixture.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -12,15 +13,18 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
+        $slugify = new Slugify();
 
         for($i = 1; $i < 31; $i++){
 
+            $title = $faker->sentence();
             $ad = new Ad();
+            $slug = $slugify->slugify( $title );
 
             $t = '<p>' . join('</p><p>' , $faker->paragraphs(5) ) . '</p>';
 
-            $ad ->setTitle( $faker->sentence() )
-                ->setSlug('dd')
+            $ad ->setTitle( $title )
+                ->setSlug( $slug )
                 ->setContent( $t )
                 ->setIntroduction( $faker->paragraph(2) )
                 ->setPrice( mt_rand( 40,1000 ) )
